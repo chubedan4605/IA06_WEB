7361c6af1619b36b360ec8f4d653c808d3aaa85a
Quang Kan <minpro111@gmail.com>
2025-12-10 21:13:52 +0700

    jwt added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Cho phép Frontend truy cập
-  app.useGlobalPipes(new ValidationPipe()); // Kích hoạt class-validator
-  await app.listen(3000);
+
+  // 1. CORS: Cho phép Credentials và link Frontend
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS
+      ? process.env.ALLOWED_ORIGINS.split(',')
+      : true,
+    credentials: true, // Quan trọng để không bị lỗi CORS khi gửi Token
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 2. PORT: Bắt buộc phải dùng process.env.PORT
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
